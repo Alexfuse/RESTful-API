82cd0cfbf94bfe8c0ce5f939824e286dd0441a3f
Alexfuse <nod.fuse@gmail.com>
2022-02-09 16:26:37 +0700

    Add post requests POJO and String

--- src/main/java/hello/Controller.java
+++ src/main/java/hello/Controller.java
@@ -1,12 +1,13 @@
 package hello;
 
+import java.net.URI;
 import java.util.concurrent.atomic.AtomicLong;
 
 import hello.models.Greeting;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import hello.models.Person;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
@@ -17,6 +18,23 @@ public class Controller {
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+
+    @PostMapping("/postbodytext")
+    public String postBody(@RequestBody String data)
+    {
+        return "Your data: "+data;
+    }
+
+    @PostMapping(
+            value = "/postbody",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Person> postBody(@RequestBody Person person) {
+        return ResponseEntity
+                .created(URI
+                        .create(String.format("/persons/%s", person.getFirstName())))
+                .body(person);
     }
 
 
